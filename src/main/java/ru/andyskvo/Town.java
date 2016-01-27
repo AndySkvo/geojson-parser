c478ce5e9b025bd0fd7f5ce95df319a0d72df702
andyskvo <andyskv@gmail.com>
2016-01-27 16:48:34 +0300

    4 поля имен населенных пунктов (Русский, Английский, Украинский,Беларусский)

--- src/main/java/ru/andyskvo/Town.java
+++ src/main/java/ru/andyskvo/Town.java
@@ -4,13 +4,20 @@ public class Town {
     private long id;
     private double latitude;
     private double longitude;
-    private String name;
+    private String nameRu;
+    private String nameEn;
+    private String nameUk;
+    private String nameBe;
 
-    public Town(long id, double latitude, double longitude, String name) {
+
+    public Town(long id, double latitude, double longitude, String nameRu, String nameEn, String nameUk, String nameBe) {
         this.id = id;
         this.latitude = latitude;
         this.longitude = longitude;
-        this.name = name;
+        this.nameRu = nameRu;
+        this.nameEn = nameEn;
+        this.nameUk = nameUk;
+        this.nameBe = nameBe;
     }
 
     public long getId() {
@@ -25,7 +32,19 @@ public class Town {
         return longitude;
     }
 
-    public String getName() {
-        return name;
+    public String getNameRu() {
+        return nameRu;
+    }
+
+    public String getNameEn() {
+        return nameEn;
+    }
+
+    public String getNameUk() {
+        return nameUk;
+    }
+
+    public String getNameBe() {
+        return nameBe;
     }
 }
