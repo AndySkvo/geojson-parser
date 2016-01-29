01eeb9a601d656725181777a49a9450a3792aefc
andyskvo <andyskv@gmail.com>
2016-01-29 09:36:27 +0300

    Реадизация вставки информации о населенных пунктах с учетом новых свойств. Добавил проверку на дубликаты населенных пунктов перед вставкой.

--- src/main/java/ru/andyskvo/DataBase.java
+++ src/main/java/ru/andyskvo/DataBase.java
@@ -1,18 +1,16 @@
 package ru.andyskvo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DataBase {
     private static Connection conn = null;
+
     public static Connection getConnection() throws SQLException {
 
         String driver = "com.mysql.jdbc.Driver";
-        String url    = "jdbc:mysql://localhost:3306/YOUR_TABLE_NAME";
-        String username = "root";
-        String password = "";
+        String url    = "jdbc:mysql://localhost:3306/DB_NAME"; //change the db name
+        String username = "root"; //change the username
+        String password = ""; //change the password
         System.setProperty(driver,"");
 
         return DriverManager.getConnection(url,username,password);
@@ -34,9 +32,22 @@ public class DataBase {
     public static void insertTown(Town town, int kp) {
         try {
             Statement statement = conn.createStatement();
-            statement.executeUpdate("INSERT INTO YOUR_TABLE_NAME VALUES (" + town.getId() + ", \"" + town.getName() + "\", " + town.getLatitude() + ", " + town.getLongitude() + ", "+ kp +")");
+            if (!isDublicate(town.getId())) {
+                statement.executeUpdate("INSERT INTO places VALUES (" + town.getId() + ", \"" + town.getNameRu() + "\", \"" + town.getNameEn() + "\",\"" + town.getNameUk() + "\",\"" + town.getNameBe() + "\", " + town.getLatitude() + ", " + town.getLongitude() + ", " + kp + ")"); //change the table name
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+    private static boolean isDublicate(long id) throws SQLException {
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery("SELECT id FROM places where id =" + id); //change table name
+            if (result.next()) {
+                return (result.getLong("id") != 0) ? true : false;
+            }
+            return false;
+    }
+
+
 }
